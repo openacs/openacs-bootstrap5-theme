5583a4a1063ff65163bb1a4e15c288f5fbd216e8
Gustaf Neumann <gustaf.neumann@wu-wien.ac.at>
2024-10-28 17:09:41 +0000

    added theme specific registry for cass class name mapping

--- tcl/resource-init.tcl
+++ tcl/resource-init.tcl
@@ -11,6 +11,20 @@ template::register_urn \
     -urn urn:ad:js:jquery \
     -resource /resources/openacs-bootstrap5-theme/js/jquery-3.7.1.min.js
 
+::template::CSS registerCSSclasses bootstrap5 {
+    action "btn btn-outline-secondary btn-sm m-1"
+    btn-default btn-outline-secondary
+    bulk-action "btn btn-outline-secondary btn-sm"
+    checkbox-inline form-check-inline
+    close btn-close
+    cog gear
+    form-action "btn btn-outline-secondary btn-sm m-1"
+    margin-form ""
+    navbar-default navbar-light
+    navbar-right ms-auto
+    print printer
+    radio-inline form-check-inline
+}
 # Local variables:
 #    mode: tcl
 #    tcl-indent-level: 4
